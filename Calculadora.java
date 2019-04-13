8a45909eeffeca1572f6275a9103d969f1d3a357
EzeVasari <ezequielvasari@gmail.com>
2019-04-13 17:15:39 -0300

    Se cambio los if/else por un switch y se arreglaron errores

--- Calculadora.java
+++ Calculadora.java
@@ -1,28 +1,41 @@
 import java.util.Scanner;
+
 public class Ahorcado {
- public static void main(String[] args) {
- Integer a;
- Integer b;
- Integer opcion;
- Integer resultado;
- do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
-restar /n 3 para multiplicar /n 4 para dividir");
- opcion = teclado.next();
- }while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
- a = teclado.next();
- System.out.println("Ingrese el segundo numero");
- b = teclado.next();
- if(opcion.equals(1)){
- resultado = a + b;
- }else if(opcion.equals(2)){
- resultado = a - b;
- }else if(opcion.equals(3)){
- resultado = a * b;
- }else{
- resultado = a / b;
- }
+	public static void main(String[] args) {
+  private Integer a;
+  private Integer b;
+  private Integer opcion;
+  private Integer resultado;
+    
+   do{
+    System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+     
+    opcion = teclado.next();
+      
+   }while(opcion < 1 && opcion > 4);
+    
+    System.out.println("Ingrese el primer numero");
+     a = teclado.next();
+    System.out.println("Ingrese el segundo numero");
+     b = teclado.next();
+ 
+     switch(opcion) {
+     case 1:
+    	 resultado = a + b;
+    	 break;
+     case 2:
+    	 resultado = a - b;
+    	 break;
+     case 3: 
+    	 resultado = a * b;
+    	 break;
+     case 4: 
+    	 if (b != 0) {
+				resultado = a / b;
+			} else {
+				System.out.println("Error, no se puede realizar"); }
+    	 break;
+     }
  System.out.println("El resultado es " + resultado.toString());
  }
 }
